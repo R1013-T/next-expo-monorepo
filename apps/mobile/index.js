c57a4afb0d813ceb36e436e5d3e5a398483ae002
RYU <takahashi.ryunosuke.job@gmail.com>
2024-12-22 01:14:43 +0900

    🚀

---
+++ apps/mobile/index.js
@@ -0,0 +1,11 @@
+import { registerRootComponent } from "expo";
+
+import App from "./App";
+
+// custom entrypoint for monorepo
+// see: https://docs.expo.dev/guides/monorepos/#change-default-entrypoint
+
+// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
+// It also ensures that whether you load the app in Expo Go or in a native build,
+// the environment is set up appropriately
+registerRootComponent(App);
